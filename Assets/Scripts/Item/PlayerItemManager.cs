71c79d487d5d36ce6e90e273b7bf8c2bff449761
Pepe Coral <coritochico@gmail.com>
2025-10-26 06:15:02 +0100

    feat: bald system

--- Assets/Scripts/Item/PlayerItemManager.cs
+++ Assets/Scripts/Item/PlayerItemManager.cs
@@ -56,10 +56,11 @@ public class PlayerItemManager : MonoBehaviour
         var cols = Physics2D.OverlapCircleAll(this.transform.position, 1.35f);
         cols = sortByDistance(cols);
 
-        Item itemToPickUp = getItemToPickUp(cols);
-        ItemTable table = isTableNear(cols);
-        ColoringStation coloringStation = isColoringStationNear(cols);
-        ItemPickUpAnimal animalPickUp = getAnimalPickUp(cols);
+        Item itemToPickUp = findComponent<ItemPickUpElement>(cols)?.item;
+        ItemTable table = findComponent<ItemTable>(cols);
+        ColoringStation coloringStation = findComponent<ColoringStation>(cols);
+        ItemPickUpAnimal animalPickUp = findComponent<ItemPickUpAnimal>(cols);
+        BaldChair baldChair = findComponent<BaldChair>(cols);
 
 
         if (!holder.hasItem() && itemToPickUp != null)
@@ -67,102 +68,111 @@ public class PlayerItemManager : MonoBehaviour
             holder.setItem(itemToPickUp);
         }
 
-        else if ( !holder.hasItem() && animalPickUp!= null)
+        else if (holder.hasItem() && holder.getItem()?.GetType() == typeof(WigItem) && baldChair != null && baldChair.isBusy) 
+        {
+            baldChair.giveWig((WigItem)holder.dropItem());
+        }
+
+        else if (!holder.hasItem() && animalPickUp != null)
         {
             holder.setItem(animalPickUp.pickAnimal());
         }
 
         else if (table != null)
         {
-            if (holder.getItem()?.GetType() == typeof(ToolItem) && holder.getItem().name == "Maquinilla" && table.getItem()?.GetType() == typeof(AnimalItem))
-            {
-                table.SetParticle();
-
-                if (table.processAndCheck())
-                {
-                    AnimalItem animalItem = (AnimalItem)table.getItem();
-                    holder.dropItem();
-                    holder.setItem(animalItem.hairToDrop);
-                    table.pickItem();
-                    table.setItem(animalItem.baldAnimal);
-                }
-            }
-            else if (holder.getItem()?.GetType() == typeof(ToolItem) && holder.getItem().name == "Pegamento" && table.getItem()?.GetType() == typeof(RawHairItem))
-            {
-                table.SetParticle();
-
-                if (table.processAndCheck())
-                {
-                    RawHairItem rawHairItem = (RawHairItem)table.getItem();
-                    holder.dropItem();
-                    table.pickItem();
-
-                    WigItem wigItem = ScriptableObject.CreateInstance<WigItem>();
-                    wigItem.sprite = rawHairItem.wigProduct.sprite;
-                    wigItem.color = WigColor.uncolored;
-
-                    table.setItem(wigItem);
-                }
-            }
-            else if (table.hasItem() && !holder.hasItem())
-            {
-                holder.setItem(table.pickItem());
-            }
-
-            else if (!table.hasItem() && holder.hasItem())
-            {
-                table.setItem(holder.dropItem());
-            }
-
+            tableLogic(table);
         }
 
         else if (coloringStation != null)
         {
-            if (holder.getItem()?.GetType() == typeof(WigItem)&& !coloringStation.hasItem()) { 
-            
-                coloringStation.setItem(holder.dropItem());
-            }
-            else if(coloringStation.hasItem()) 
-            {
+            coloringStationLogic(coloringStation);
+        }
 
-                WigItem wig = (WigItem)coloringStation.getItem();
-                if (wig.color != coloringStation.color)
-                {
-                    coloringStation.SetParticle();
+        else if (holder.hasItem() && holder.getItem().GetType() != typeof(AnimalItem) && isTagNear(cols, "TrashCan"))
+        {
+            holder.dropItem();
+        }
 
-                    if (coloringStation.processAndCheck())
-                    {
-                        wig.color = coloringStation.color;
-                        coloringStation.updateColor();
-                    }
-                }
-                else
-                {
-                    holder.setItem(coloringStation.pickItem());
-                }
-
-            }
-        }   
-
-        else if (holder.hasItem() && isTrashCanNear(cols))
+        else if (holder.hasItem() && holder.getItem().GetType() == typeof(AnimalItem) && isTagNear(cols, "Trasportin"))
         {
             holder.dropItem();
         }
     }
 
-    private ItemPickUpAnimal getAnimalPickUp(Collider2D[] cols)
+    private void coloringStationLogic(ColoringStation coloringStation)
     {
-        foreach (Collider2D col in cols)
+        if (holder.getItem()?.GetType() == typeof(WigItem) && !coloringStation.hasItem())
         {
-            ItemPickUpAnimal animal;
-            if (col.transform.TryGetComponent<ItemPickUpAnimal>(out animal))
+
+            coloringStation.setItem(holder.dropItem());
+        }
+        else if (coloringStation.hasItem())
+        {
+
+            WigItem wig = (WigItem)coloringStation.getItem();
+            if (wig.color != coloringStation.color)
             {
-                return animal;
+                coloringStation.SetParticle();
+
+                if (coloringStation.processAndCheck())
+                {
+                    wig.color = coloringStation.color;
+                    coloringStation.updateColor();
+                }
+            }
+            else
+            {
+                holder.setItem(coloringStation.pickItem());
+            }
+
+        }
+    }
+
+    private void tableLogic(ItemTable table)
+    {
+        if (holder.getItem()?.GetType() == typeof(ToolItem) && holder.getItem().name == "Maquinilla" && table.getItem()?.GetType() == typeof(AnimalItem))
+        {
+            table.SetParticle();
+
+            if (table.processAndCheck())
+            {
+                AnimalItem animalItem = (AnimalItem)table.getItem();
+                holder.dropItem();
+                holder.setItem(animalItem.hairToDrop);
+                table.pickItem();
+                table.setItem(animalItem.baldAnimal);
             }
         }
+        else if (holder.getItem()?.GetType() == typeof(ToolItem) && holder.getItem().name == "Pegamento" && table.getItem()?.GetType() == typeof(RawHairItem))
+        {
+            table.SetParticle();
 
-        return null;
+            if (table.processAndCheck())
+            {
+                RawHairItem rawHairItem = (RawHairItem)table.getItem();
+                holder.dropItem();
+                table.pickItem();
+
+                WigItem wigItem = ScriptableObject.CreateInstance<WigItem>();
+                wigItem.sprite = rawHairItem.wigProduct.sprite;
+                wigItem.wigType = rawHairItem.wigProduct.wigType;
+                wigItem.wigForBald = rawHairItem.wigProduct.wigForBald;
+                wigItem.color = WigColor.uncolored;
+
+                table.setItem(wigItem);
+            }
+        }
+        else if (table.hasItem() && !holder.hasItem())
+        {
+            holder.setItem(table.pickItem());
+        }
+
+        else if (!table.hasItem() && holder.hasItem())
+        {
+            table.setItem(holder.dropItem());
+        }
     }
+
 
     private Collider2D[] sortByDistance(Collider2D[] cols)
     {
@@ -170,55 +180,12 @@ public class PlayerItemManager : MonoBehaviour
 
     }
 
-    private Item getItemToPickUp(Collider2D[] cols)
+    private bool isTagNear(Collider2D[] cols, String tag)
     {
 
         foreach (Collider2D col in cols)
         {
-            ItemPickUpElement itemBox;
-            if (col.transform.TryGetComponent<ItemPickUpElement>(out itemBox))
-            {
-                return itemBox.item;
-            }
-        }
-
-        return null;
-    }
-
-    private ColoringStation isColoringStationNear(Collider2D[] cols)
-    {
-        ColoringStation coloringStation;
-        foreach (Collider2D col in cols)
-        {
-            if (col.transform.TryGetComponent<ColoringStation>(out coloringStation))
-            {
-                return coloringStation;
-            }
-        }
-
-        return null;
-    }
-
-    private ItemTable isTableNear(Collider2D[] cols)
-    {
-        foreach(Collider2D col in cols)
-        {
-            ItemTable table;
-            if(col.transform.TryGetComponent<ItemTable>(out table))
-            {
-                return table;
-
-            }
-        }
-        return null;
-    }
-
-    private bool isTrashCanNear(Collider2D[] cols)
-    {
-
-        foreach (Collider2D col in cols)
-        {
-            if (col.transform.tag == "TrashCan")
+            if (col.transform.tag == tag)
             {
                 return true;
             }
@@ -226,6 +193,23 @@ public class PlayerItemManager : MonoBehaviour
 
         return false;
     }
+
+
+    private T findComponent<T>(Collider2D[] cols) 
+    {
+        foreach (Collider2D col in cols)
+        {
+            T component;
+            if (col.transform.TryGetComponent<T>(out component))
+            {
+                return component;
+
+            }
+        }
+        return default(T);
+    }
+
+    
 
 
 
